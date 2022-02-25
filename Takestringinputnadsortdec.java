ced3f0b6addeac69a2efc2191a28adbce87ad6b0
IAMBOTSB <36182266+IAMBOTSB@users.noreply.github.com>
2022-02-25 20:39:45 +0530

    Create Takestringinputnadsortdec.java

---
+++ Takestringinputnadsortdec.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Takestringinputnadsortdec
+{
+    public static void main(String[] args)
+    {
+        String books[]=new String[5];
+        Scanner sc = new Scanner(System.in);
+
+        for(int i=0;i<5;i++){
+            books[i]=sc.nextLine();
+        }
+        Arrays.sort(books);
+
+        for(int j=0;j<books.length;j++){
+            System.out.println(books[j]);
+        }
+    }
+}
